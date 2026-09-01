62db885123cb55c729fd130ecddc321dec69b3a4
Salahudeen-femi1 <102353790+Salahudeen-femi1@users.noreply.github.com>
2026-09-01 18:43:20 +0100

    Fix verifyEmailService to use GET method for email verification

--- src/helper/authService.ts
+++ src/helper/authService.ts
@@ -16,7 +16,7 @@ interface VerifyEmailProp {
 }
 
 export const verifyEmailService = async (value: VerifyEmailProp) => {
-    const response = await api.post(`/v1/auth/verify-email?token=${value.token}`)
+    const response = await api.get(`/v1/auth/verify-email?token=${value.token}`)
     console.log(response)
     return response.data.data
 }
